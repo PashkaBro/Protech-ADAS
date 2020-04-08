9885ef503aad41b8cb72f5573cdecb66f0da6b0b
PashkaBro <veenoompoltava@gmail.com>
2020-04-07 17:59:28 -0700

    first commit

---
+++ src/Content.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+
+    <Header/>
+
+    
+<Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Nav from 'react-bootstrap/Nav'
+
+function Header() {
+  return (
+    <div className="Header">
+
+      <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+    </div>
+  );
+}
+
+export default Header;
